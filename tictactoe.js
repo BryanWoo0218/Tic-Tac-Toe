9eaec416a38a4182201ea4b460adb16213b422df
BryanWoo <c.bryan.woo@gmail.com>
2019-10-17 18:19:03 +1100

    All done except timer

--- tictactoe.js
+++ tictactoe.js
@@ -4,8 +4,11 @@ var clickCounter = 0
 var gameInfo = document.querySelector('.info-display')
 var restartBtn = document.querySelector('.restart')
 var wonIndicate = document.querySelector('.strike')
+var wonIndicateW = document.querySelector('.strikeW')
 var winnerInfo = document.querySelector('.winner')
 var gameBoard = document.querySelector('main')
+var player1Turn = document.querySelector('.player1')
+var player2Turn = document.querySelector('.player2')
 
 
 var clicked = function(event){
@@ -15,11 +18,15 @@ var clicked = function(event){
       event.target.classList.add('ticked')
       event.target.textContent = 'X' 
       gameInfo.textContent = 'Play2, your turn'
+      player2Turn.classList.add('active')
+      player1Turn.classList.remove('active')
       winningChecker('X')
     } else {
       event.target.classList.add('ticked')
       event.target.textContent = 'O'
       gameInfo.textContent = 'Play1, your turn'
+      player1Turn.classList.add('active')
+      player2Turn.classList.remove('active')
       winningChecker('O')
     }
   } else {
@@ -43,7 +50,11 @@ var winningChecker =function(playerMark) {
   for (var i = 0; i < winningCombination.length; i++) {
     var winningLine = [winningCombination[i][0].textContent, winningCombination[i][1].textContent, winningCombination[i][2].textContent]
     if ( winningLine.join('') === checkId ){
+      if(i === 0 || i === 5 ||i === 6) {
+      wonIndicateW.classList.add(`strikeW-${i}`)
+      } else {
       wonIndicate.classList.add(`strike-${i}`)
+      } 
       finish(playerMark)
       i = 8
     } else if(clickCounter === 9){
@@ -56,12 +67,18 @@ var finish = function (playerMark) {
   if (playerMark === 'X') {
     winnerInfo.textContent = 'Player1 is'
     gameInfo.textContent = 'Winner!'
+    player1Turn.classList.add('active')
+    player2Turn.classList.remove('active')
   } else if(playerMark === 'O'){
     winnerInfo.textContent = 'Player2 is'
     gameInfo.textContent = 'Winner!'
+    player1Turn.classList.remove('active')
+    player2Turn.classList.add('active')
   } else if(playerMark === 'N'){
     winnerInfo.textContent = 'DRAW!'
     gameInfo.textContent = "Let's Play Again!"
+    player1Turn.classList.remove('active')
+    player2Turn.classList.remove('active')
   } else {}
 }
 
@@ -73,10 +90,13 @@ var reset = function (){
   clickCounter = 0
   gameInfo.textContent = 'Waiting...For Start'
   winnerInfo.textContent = ''
+  player1Turn.classList.remove('active')
+  player2Turn.classList.remove('active')
   for (var i = 0; i < boxes.length; i++) {
     boxes[i].textContent = ''
     boxes[i].classList.remove('ticked')
     wonIndicate.classList.remove(`strike-${i}`)
+    wonIndicateW.classList.remove(`strikeW-${i}`)
   }
 }
 
